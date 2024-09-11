8656beec08110013d2b81b8b59bdd099f3e1e37b
mmagician <marcin.gorny.94@protonmail.com>
2024-09-11 08:17:48 +0200

    avoid code duplication in the prover<>verifier benching

--- benches/prove_verify.rs
+++ benches/prove_verify.rs
@@ -28,60 +28,38 @@ fn generate_fibonacci_circuit(steps: usize) -> ArithmeticCircuit<Fr> {
     circuit
 }
 
-pub fn prover_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("prover");
-    for size in [10, 12, 14].iter() {
-        group.bench_with_input(
-            BenchmarkId::new("prove_fibonacci", format!("1 << {}", size)),
-            size,
-            |b, &size| {
-                let steps = 1 << size;
-                let circuit = generate_fibonacci_circuit(steps);
-                let output_node = circuit.last();
-                let ligero_circuit =
-                    LigeroCircuit::new(circuit, vec![output_node], DEFAULT_SECURITY_LEVEL);
-                let sponge: PoseidonSponge<Fr> = test_sponge();
-                let mt_params = LigeroMTTestParams::new();
+fn bench_prove_verify(c: &mut Criterion) {
+    let mut group = c.benchmark_group("ligero");
+    for size in [10].iter() {
+        let steps = 1 << size;
+        let circuit = generate_fibonacci_circuit(steps);
+        let output_node = circuit.last();
+        let ligero_circuit = LigeroCircuit::new(circuit, vec![output_node], DEFAULT_SECURITY_LEVEL);
+        let sponge: PoseidonSponge<Fr> = test_sponge();
+        let mt_params = LigeroMTTestParams::new();
 
-                b.iter(|| {
-                    ligero_circuit.prove_with_labels(
-                        vec![("f_0", Fr::from(1)), ("f_1", Fr::from(1))],
-                        &mt_params,
-                        &mut sponge.clone(),
-                    )
-                })
-            },
-        );
-    }
-    group.finish();
-}
-
-pub fn verifier_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("verifier");
-    for size in [10, 12, 14].iter() {
-        group.bench_with_input(
-            BenchmarkId::new("verify_fibonacci", format!("1 << {}", size)),
-            size,
-            |b, &size| {
-                let steps = 1 << size;
-                let circuit = generate_fibonacci_circuit(steps);
-                let output_node = circuit.last();
-                let ligero_circuit =
-                    LigeroCircuit::new(circuit, vec![output_node], DEFAULT_SECURITY_LEVEL);
-                let sponge: PoseidonSponge<Fr> = test_sponge();
-                let mt_params = LigeroMTTestParams::new();
-                let proof = ligero_circuit.prove_with_labels(
+        group.bench_function(BenchmarkId::new("prove_fibonacci", size), |b| {
+            b.iter(|| {
+                ligero_circuit.prove_with_labels(
                     vec![("f_0", Fr::from(1)), ("f_1", Fr::from(1))],
                     &mt_params,
                     &mut sponge.clone(),
-                );
+                )
+            })
+        });
 
-                b.iter(|| ligero_circuit.verify(&proof, &mt_params, &mut sponge.clone()))
-            },
+        let proof = ligero_circuit.prove_with_labels(
+            vec![("f_0", Fr::from(1)), ("f_1", Fr::from(1))],
+            &mt_params,
+            &mut sponge.clone(),
         );
+
+        group.bench_function(BenchmarkId::new("verify_fibonacci", size), |b| {
+            b.iter(|| ligero_circuit.verify(&proof, &mt_params, &mut sponge.clone()))
+        });
     }
     group.finish();
 }
 
-criterion_group!(benches, prover_benchmark, verifier_benchmark);
+criterion_group!(benches, bench_prove_verify);
 criterion_main!(benches);
